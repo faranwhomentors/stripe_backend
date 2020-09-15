e955a9b8910b14c882fcc385c26adc332035bbe5
Glitch (hello-webpage) <none>
2020-09-15 04:42:28 +0000

    🖐🐪 Checkpoint ./server.js:11165748/1015

--- server.js
+++ server.js
@@ -3,24 +3,33 @@ const express = require("express");
 const app = express();
 const { resolve } = require("path");
 // This is your real test secret API key.
-const stripe = require("stripe")("sk_test_jOhmfBMo8j7LMZ3fippevbXw00no0OnVvr");
+const stripe = require("stripe")(process.env.secret_key);
+
 app.use(express.static("."));
 app.use(express.json());
-const calculateOrderAmount = items => {
-  // Replace this constant with a calculation of the order's amount
-  // Calculate the order total on the server to prevent
-  // people from directly manipulating the amount on the client
-  return 1400;
-};
-app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
+
+app.post("/checkout", async (req, res) => {
+  // const { items } = req.body;
+  console.log('hi');
+  const customer = await stripe.customers.create();
+  
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: "usd"
+    amount: 100,
+    currency: "usd",
+    customer: customer.id
   });
+  
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    {customer: customer.id},
+    {api_version: '2020-08-27'}
+  );  
+  
   res.send({
-    clientSecret: paymentIntent.client_secret
+    clientSecret: paymentIntent.client_secret,
+    customerID: customer.id,
+    ephemeralKeySecret: ephemeralKey.secret
   });
 });
-app.listen(4242, () => console.log('Node server listening on port 4242!'));
+
+app.listen(3000, () => console.log('Node server listening on port 4242!'));
