c8f1a90235112c783772fee8223b788e2b9a41e2
Glitch (hello-webpage) <none>
2020-12-06 04:09:00 +0000

    🌇🙎 Checkpoint ./README.md:11165748/115 ./server.js:11165748/2615

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Welcome to Glitch
+An example backend for Stripe's drop-in mobile integration
 =================
 
 Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 const { resolve } = require("path");
@@ -8,8 +7,8 @@ const stripe = require("stripe")(process.env.secret_key);
 app.use(express.static("."));
 app.use(express.json());
 
+// An endpoint for your checkout 
 app.post("/checkout", async (req, res) => {
-
   let customer_type = req.body.customer
   console.log(req.body)
   let customer
@@ -21,6 +20,39 @@ app.post("/checkout", async (req, res) => {
       );
   }
   
+  // Create a PaymentIntent with the order amount, currency, and customer
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 100,
+    currency: "usd",
+    customer: customer.id
+  });
+  
+  // Create a 
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    {customer: customer.id},
+    {apiVersion: '2020-08-27'}
+  );  
+  
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+    customerID: customer.id,
+    ephemeralKey: ephemeralKey.secret
+  });
+});
+
+// An endpoint for the test playground
+app.post("/checkout_playground", async (req, res) => {
+
+  let customer_type = req.body.customer
+  console.log(req.body)
+  let customer
+  if (customer_type == "new") {
+      customer = await stripe.customers.create();
+  } else if (customer_type == "saved") {
+      customer = await stripe.customers.retrieve(
+        'cus_GWUuPERgJF44Dm'
+      );
+  }
   
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
@@ -40,5 +72,6 @@ app.post("/checkout", async (req, res) => {
     ephemeralKey: ephemeralKey.secret
   });
 });
+
 
 app.listen(3000, () => console.log('Node server listening on port 4242!'));
